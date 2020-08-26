bd6d629ee1fda814baa52e9c671843b161184177
canizales99 <56712245+canizales99@users.noreply.github.com>
2020-08-26 17:20:09 -0600

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -9,7 +9,7 @@ Media_citric_acid = dataframe.citric_acid.median()
 Media_residual_sugar = dataframe.residual_sugar.median()
 
 #Determina media alta y media baja
-def funcion(x,y,z):
+def Media_Alta_Y_Baja(x,y,z):
     for i, x in enumerate(x):
         if x >= z:
             dataframe.loc[i,y] = 'Media Alta'
@@ -18,7 +18,7 @@ def funcion(x,y,z):
     print(dataframe.groupby(y).quality.mean())
 
 #(dataframe.columna, 'nombre bajo el que se muestra', Media_columna)
-funcion(dataframe.alcohol, 'alcohol', Media_Alcohol)
-funcion(dataframe.pH, 'pH', Media_pH)
-funcion(dataframe.citric_acid, 'citric_acid', Media_citric_acid)
-funcion(dataframe.residual_sugar, 'residual_sugar', Media_residual_sugar)
+Media_Alta_Y_Baja(dataframe.alcohol, 'alcohol', Media_Alcohol)
+Media_Alta_Y_Baja(dataframe.pH, 'pH', Media_pH)
+Media_Alta_Y_Baja(dataframe.citric_acid, 'citric_acid', Media_citric_acid)
+Media_Alta_Y_Baja(dataframe.residual_sugar, 'residual_sugar', Media_residual_sugar)
